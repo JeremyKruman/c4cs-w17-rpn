3abef770485c2539380e97a87d64e5815c92da56
Jeremy <jkruman@jeremy398-VirtualBox.none>
2017-03-10 16:08:43 -0500

    attendance

--- rpn.py
+++ rpn.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-	pass
+	exIn = arg.split(' ')
+	stack = []
+	ops = ['+', '-']
+	val = 0
+	for ex in exIn:
+		if (ex.isnumeric()):
+			stack.append(int(ex))
+		elif (ex in ops):
+			if (len(stack) < 2):
+				print('Error: Operation defined before valid numbers')
+			else:
+				if(ex == '+'):
+					val = 0
+					while (len(stack) > 0):
+						val = val + stack.pop()
+					stack.append(val)
+				elif(ex == '-'):
+					stack.reverse()
+					val = stack.pop()
+					while(len(stack) > 0):
+						val = val - stack.pop()
+					stack.append(val)
+		else:
+			print('Error: Invalid character entered')
+			break
+	return(val)
+
+
+
 
 def main():
 	while True:
